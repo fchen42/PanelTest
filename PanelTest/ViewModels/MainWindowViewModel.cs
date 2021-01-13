02d23513231e121a88cc6360cee4047a24493ae0
Feng Chen <chenf@outlook.com>
2021-01-13 17:31:51 +0800

    initial commit

---
+++ PanelTest/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,67 @@
+﻿using PropertyChanged;
+using System.Windows;
+using System.Windows.Input;
+
+namespace PanelTest.ViewModels
+{
+    [AddINotifyPropertyChangedInterface]
+    public class MainWindowViewModel
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// 当前管理窗体的状态。
+        /// </summary>
+        public WindowState ViewWindowState { get; set; }
+
+        /// <summary>
+        /// 窗口标题。
+        /// </summary>
+        public string WindowTitle { get; set; } = "PanelTest";
+
+        /// <summary>
+        /// 标题栏高度。
+        /// </summary>
+        public int TitleBarHeight { get; set; } = 36;
+
+        /// <summary>
+        /// 窗口外边框大小。
+        /// </summary>
+        public int OuterMarginSize { get; set; } = 10;
+
+        /// <summary>
+        /// 可拖拽缩放区域大小。
+        /// </summary>
+        public int ResizeAreaSize { get; set; } = 6;
+
+        /// <summary>
+        /// 可拖拽缩放边框整体大小。
+        /// </summary>
+        public int ResizeBorderSize => OuterMarginSize + ResizeAreaSize;
+
+        #endregion
+
+        #region Constructor
+
+        public MainWindowViewModel()
+        {
+            ViewWindowState = WindowState.Normal;
+        }
+
+        #endregion
+
+        #region Commands
+
+        /// <summary>
+        /// 窗口最大化命令。
+        /// </summary>
+        public ICommand WindowMaximizeCommand => new RelayCommand(() => ViewWindowState = WindowState.Maximized);
+
+        /// <summary>
+        /// 窗口最小化命令。
+        /// </summary>
+        public ICommand WindowMinimizeCommand => new RelayCommand(() => ViewWindowState = WindowState.Minimized);
+
+        #endregion
+    }
+}
---
+++ PanelTest/ViewModels/RelayCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace PanelTest.ViewModels
+{
+    public class RelayCommand : ICommand
+    {
+        #region Private Members
+
+        private readonly Action _action;
+
+        #endregion
+
+        #region Public Events
+
+        public event EventHandler CanExecuteChanged = (sender, args) => { };
+
+        #endregion
+
+        #region Constructor
+
+        public RelayCommand(Action action)
+        {
+            _action = action;
+        }
+
+        #endregion
+
+        #region Command Methods
+
+        public bool CanExecute(object parameter) => true;
+
+        public void Execute(object parameter) => _action();
+
+        #endregion
+
+    }
+}
