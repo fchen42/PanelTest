0cc0e988ccb709a6b2c7d5d5404bd938eaf3cc8c
Feng Chen <chenf@outlook.com>
2021-01-15 16:15:01 +0800

    修改设计布局&配色

---
+++ PanelTest/Views/Controls/NavigationDrawer.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace PanelTest.Views.Controls
+{
+    /// <summary>
+    /// NavigationDrawer.xaml 的交互逻辑
+    /// </summary>
+    public partial class NavigationDrawer : UserControl
+    {
+        public NavigationDrawer()
+        {
+            InitializeComponent();
+        }
+    }
+}
